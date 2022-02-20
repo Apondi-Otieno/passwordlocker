92ea6d642f39d8ae98c872437599507b501fe7c7
Apondi Otieno <apondotii@gmail.com>
2022-02-20 22:24:35 +0300

    this will help the user copy and paste passwords matching the account name

--- user.py
+++ user.py
@@ -23,7 +23,7 @@ class user:
     def __init__(self, username,password):
         self.username = username
         self.password = password
-        
+
 # this will save new user details
 
     def save_user(self):
@@ -36,3 +36,62 @@ class user:
 
       def display_user(cls):
           return cls.user_list
+
+#this will delete user details
+
+      def delete_user(self):
+
+    user.user_list.remove(self)
+
+
+# new class
+
+class Credentials():
+
+    credentials_list = []
+
+    @classmethod
+    def verify_user(cls,username,password):
+
+        a_user =""
+for user in User.user_list:
+    if(user,username==username and user.password==password):
+        a_user==user.username
+        return a_user
+
+
+# storing user details
+
+        def __init__(self,account,username,password):
+
+            self.account = account
+            self.username = username
+            self.password = password
+
+        def save_details(self):
+
+            Credentials.credentials_list.append(self)
+
+        def delete_credentials(self):
+
+            Credentials.credentials_list.remove(self)
+
+
+   @classmethod
+
+ # this method takes in account name and returns a matching password
+
+ def find_credentials(cls,account):
+     for Credentials in cls.credentials_list:
+         if credential.account == account:
+             return credential
+
+    @classmethod
+
+#this method will let the user copy the passwords from
+
+def copy_password(cls,account):
+    found_credentials =Credentials.find_credentials(account)
+    pyperclip.copy(found_credentials.password)
+    password_copied=pyperclip.paste()
+    print (password_copied)
